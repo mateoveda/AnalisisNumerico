52d4b47c9eb255727da7dc53d293f843399a93ae
tobiasvnicolas <2215878@ucc.edu.ar>
2023-08-30 11:52:38 -0400

    Ya estan derivadas las funciones (antes no estaban ._.XD)

--- Ecuaciones/Derivadas.cpp
+++ Ecuaciones/Derivadas.cpp
@@ -17,20 +17,20 @@ double Derivadas::der1(double _x){
 
 double Derivadas::der2(double _x){
     x=_x;
-    return (-0.874*pow(x,2)+1.750*x+2.627);
+    return (-0.874*2*x+1.750);
 }
 
 double Derivadas::der3(double _x){
     x=_x;
-    return (-23.330+79.350*x-88.090*pow(x,2)+41.600*pow(x,3)-8.680*pow(x,4)+0.658*pow(x,5));
+    return (-23.330+79.350-88.090*2*x+41.600*3*pow(x,2)-8.680*4*pow(x,3)+0.658*5*pow(x,4));
 }
 
 double Derivadas::der4(double _x){
     x=_x;
-    return (log(x)-5);
+    return (1/x);
 }
 
 double Derivadas::der5(double _x){
     x=_x;
-    return ((1-0.6*x)/-x);
+    return (1/pow(x,2));
 }
